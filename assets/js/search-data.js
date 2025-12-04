ef245d418e731f456e48dc4680c417b20c4ed84e
mozhgantaghavi <124168596+mozhgantaghavi@users.noreply.github.com>
2025-12-04 20:19:28 +0000

    Deploying to gh-pages from @ mozhgantaghavi/mozhgantaghavi.github.io@56d7e3b1c25706a6afd01f2a8d0a486c6cd415e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-courses",
           title: "Courses",
-          description: "This is my course",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/Courses/";
